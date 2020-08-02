2e1ce51889761716cc8a4ecae1ec60cfa778f770
Davide Palazzo <kans@hotmail.it>
2020-08-02 12:00:55 +1000

    Add a catch-all route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser'
 
 import { exampleRouter } from './routes/example'
 import { ascyncExampleRouter } from './routes/async-example'
+import { ROUTE_PATHS } from './routes'
 
 const app = express()
 
@@ -11,5 +12,9 @@ app.use(json())
 
 app.use(exampleRouter)
 app.use(ascyncExampleRouter)
+
+app.all(ROUTE_PATHS.ALL, async (req, res) => {
+  res.status(404).send('<h1>404 - Page not found</h1>')
+})
 
 export { app }
