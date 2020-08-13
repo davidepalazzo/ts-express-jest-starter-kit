0422addac9ba33bac1fb7efcca8b5106cfb91185
Davide Palazzo <kans@hotmail.it>
2020-08-13 18:42:40 +1000

    Middlewares folder

--- src/routes/example.ts
+++ src/routes/example.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from 'express'
 
 import { ROUTE_PATHS } from '.'
+import { customMiddleware } from '../middlewares'
 
 const router = express.Router()
 
-router.get(ROUTE_PATHS.EXAMPLE, (req: Request, res: Response) => {
-  res.send({
-    data: 'Example route',
-  })
-})
+router.get(
+  ROUTE_PATHS.EXAMPLE,
+  customMiddleware,
+  (req: Request, res: Response) => {
+    res.send({
+      data: 'Example route',
+    })
+  }
+)
 
 export { router as exampleRouter }
